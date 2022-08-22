ce6d94b25163a662ca112652515e0f401db9491e
Greg Olmschenk <greg@olmschenk.com>
2022-08-22 19:08:39 -0400

    Create problematic over grid scene with issues due to computational intensity of opacity layers in 3D

--- neural_network_explanation_presentation_animations/main_scene.py
+++ neural_network_explanation_presentation_animations/main_scene.py
@@ -5,7 +5,7 @@ from typing import List
 import numpy as np
 from manim import Scene, Circle, PINK, Create, config, ImageMobject, RIGHT, Rectangle, FadeIn, FadeOut, BLACK, \
     ReplacementTransform, FadeTransform, DOWN, Dot, ThreeDScene, Sphere, PI, TAU, RED, VGroup, Line3D, IN, Surface, \
-    Mobject
+    Mobject, OUT, LaggedStart
 
 
 class MainScene(ThreeDScene):
@@ -16,9 +16,9 @@ class MainScene(ThreeDScene):
         number_of_pixels = 10
         assert number_of_pixels % 2 == 0
         # 32 seems to be the default?
-        line3d_resolution = 10
-        sphere_resolution = 10
-        xy_rectangle_resolution = (10, 10)
+        line3d_resolution = 5
+        sphere_resolution = 5
+        xy_rectangle_resolution = (5, 5)
         skip_animations = True
         # Create image.
         self.next_section(skip_animations=skip_animations)
@@ -53,6 +53,16 @@ class MainScene(ThreeDScene):
             neuron_kernel_.set_color(RED)
             return neuron_kernel_
 
+        def create_neuron_output(x_center, y_center) -> XyRectangle:
+            size = 0.9 * pixel_size
+            x = x_center - (size / 2)
+            y = y_center - (size / 2)
+            neuron_kernel_ = XyRectangle(x=x, y=y, x_size=size, y_size=size, z=2,
+                                         resolution=xy_rectangle_resolution)
+            neuron_kernel_.set_opacity(0.5)
+            neuron_kernel_.set_color(RED)
+            return neuron_kernel_
+
         def create_neuron(x, y):
             neuron_z_position = 1
             neuron_ = Sphere(
@@ -73,21 +83,39 @@ class MainScene(ThreeDScene):
         for pixel_index_x in range(1, number_of_pixels-1):
             neurons.append([])
             neuron_kernels.append([])
+            neuron_outputs.append([])
             for pixel_index_y in range(1, number_of_pixels-1):
                 x_position, y_position = pixel_index_to_position(pixel_index_x, pixel_index_y)
                 neurons[pixel_index_x - 1].append(create_neuron(x_position, y_position))
                 neuron_kernels[pixel_index_x - 1].append(create_neuron_kernel(x_position, y_position))
+                neuron_outputs[pixel_index_x - 1].append(create_neuron_output(x_position, y_position))
 
         self.next_section(skip_animations=False)
-        self.play(FadeIn(neurons[0][0], shift=IN), FadeIn(neuron_kernels[0][0], shift=IN))
+        self.play(FadeIn(neurons[0][0], shift=IN))
+        # Show the neuron kernel.
+        self.next_section(skip_animations=False)
+        self.play(FadeIn(neuron_kernels[0][0], shift=IN))
         # Move to angled view.
         self.next_section(skip_animations=False)
         self.move_camera(1.2 * math.tau / 8, -3 * math.tau / 8)
+        # Show the neuron output.
+        self.next_section(skip_animations=False)
+        self.play(FadeIn(neuron_outputs[0][0], shift=OUT))
         # Add another neuron
         self.next_section(skip_animations=False)
         self.play(FadeIn(neurons[1][0], shift=IN), FadeIn(neuron_kernels[1][0], shift=IN))
+        # Add remaining neurons
+        self.next_section(skip_animations=False)
+        add_neuron_animations = []
+        for x in range(len(neurons)):
+            for y in range(len(neurons[x])):
+                if (x == 0 or x == 1) and y == 0:
+                    continue  # Already added neuron and kernel.
+                add_neuron_animations.append(FadeIn(neurons[x][y], shift=IN))
+                add_neuron_animations.append(FadeIn(neuron_kernels[x][y], shift=IN))
+        self.play(LaggedStart(*add_neuron_animations))
 
-        self.next_section(skip_animations=skip_animations)
+        self.next_section(skip_animations=False)
 
         self.wait(1)
 
